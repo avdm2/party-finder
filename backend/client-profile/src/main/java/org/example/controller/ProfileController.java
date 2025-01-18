2becb5bcef91b9ebc33c2017425de0fa3eb18201
agnuscrucifixion <nikitabysko123@gmail.com>
2025-01-18 12:57:13 +0300

    Контроллер профиля клиента

--- backend/client-profile/src/main/java/org/example/controller/ProfileController.java
+++ backend/client-profile/src/main/java/org/example/controller/ProfileController.java
@@ -1,11 +1,27 @@
 package org.example.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.example.models.dto.ClientDTO;
+import org.example.models.dto.ProfileDTO;
+import org.example.service.ProfileService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/client-service/profile")
 @RequiredArgsConstructor
 public class ProfileController {
+
+    private final ProfileService profileService;
+
+    @PostMapping
+    public UUID createProfile(@RequestBody ClientDTO clientDTO) {
+        return profileService.createProfile(clientDTO);
+    }
+
+    @GetMapping("/{id}")
+    public ProfileDTO getEvent(@PathVariable UUID id) {
+        return profileService.getProfile(id);
+    }
 }
