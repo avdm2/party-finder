1bc568a3ff07fb46b6ad3f16f76b44cdf3217661
artem <avdmitriev_2@edu.hse.ru>
2025-01-27 22:27:28 +0300

    Таблицы для auth-service

--- db_tables/db_tables.sql
+++ db_tables/db_tables.sql
@@ -1,4 +1,4 @@
-CREATE TABLE event_instance
+create table event.event_instance
 (
     id          uuid default gen_random_uuid() not null
         primary key,
@@ -7,7 +7,7 @@ CREATE TABLE event_instance
     description varchar                        not null
 );
 
-CREATE TABLE client.client
+create table client.client
 (
     id         uuid default gen_random_uuid() primary key,
     name       varchar not null,
@@ -15,11 +15,28 @@ CREATE TABLE client.client
     birth_date date    not null
 );
 
-CREATE TABLE client.profile
+create table client.profile
 (
     id           uuid default gen_random_uuid() primary key,
     client_id    uuid      not null unique references client.client (id) on delete cascade,
     is_confirmed boolean   not null,
     created_time timestamp not null,
     updated_time timestamp not null
+);
+
+create table auth."user"
+(
+    id        uuid not null primary key,
+    firstname varchar(255),
+    lastname  varchar(255),
+    password  varchar(255),
+    username  varchar(255)
+        constraint uksb8bbouer5wak8vyiiy4pf2bx unique
+);
+
+create table auth.user_roles
+(
+    user_id uuid not null
+        constraint fk55itppkw3i07do3h7qoclqd4k references "user",
+    role    varchar(255)
 );
